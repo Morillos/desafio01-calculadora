01520c8c790ec4a2dcbdf7e253f87be5d7c76f04
Murilo <murilobarcelos103@gmail.com>
2024-11-21 14:57:50 -0300

    initial_commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #00AAFF;
+    color: white;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    border-radius: 1rem;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #CACACA;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #FFF;
+    padding: 2px;
+    width: 50%;
+`
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 2px;
+    justify-content: space-between;
+    align-items: center;
+    margin: 2px 0;
+`
